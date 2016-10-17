df7cf7decf359542fe559e8930acbf459d6f9cec
Diana Man <dianaman82@gmail.com>
2016-10-17 23:26:54 +0100

    addded stylesheet but can't make it work in browser

--- controller.rb
+++ controller.rb
@@ -12,10 +12,11 @@ end
 get '/address' do
   content_type(:json)
   address = Address.new(params[:address],params[:building],params[:postcode],params[:phone])
+  word_formatter = WordFormatter.new(params[:postcode]).upcase
   results = {
     address: address.address(),
     building: address.building(),
-    postcode: address.postcode().upcase,
+    postcode: address.postcode(),
     phone: address.phone()
   }
   results.to_json
