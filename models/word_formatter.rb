0f32349d0af048df880875614af08598a0937111
Diana Man <dianaman82@gmail.com>
2016-10-17 23:50:10 +0100

    added camelcase method

--- models/word_formatter.rb
+++ models/word_formatter.rb
@@ -2,14 +2,18 @@
 class WordFormatter
 
 
-  attr_accessor(:postcode)
+  attr_accessor(:to_format)
 
-  def initialize(postcode)
-    @postcode = postcode
+  def initialize(to_format)
+    @to_format = to_format
   end
 
   def upcase()
-    return @postcode.upcase
+    return @to_format.upcase
+  end
+
+  def camelcase
+    return @to_format.split.collect(&:capitalize).join
   end
 
 end
--- specs/word_formatter_specs.rb
+++ specs/word_formatter_specs.rb
@@ -6,11 +6,16 @@ require( 'pry-byebug')
 class WordFormatterTest < Minitest::Test
 
   def setup
-    @postcode = WordFormatter.new('e13 zqf')
+    @to_format1 = WordFormatter.new('e13 zqf')
+    @to_format2 = WordFormatter.new("code clan")
   end
 
   def test_upcase
-    assert_equal( 'E13 ZQF',@postcode.upcase)
+    assert_equal( 'E13 ZQF',@to_format1.upcase)
+  end
+
+  def test_camelcase
+    assert_equal( 'CodeClan', @to_format2.camelcase)
   end
 
 end
